fae09630ee6fdee83b5aa6c1b40d01bf87c29764
aventador <303639161@qq.com>
2017-11-29 23:38:27 -0500

    Added more things

---
+++ conFusion/src/app/menu/menu.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  dishes: Dish[] = 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
