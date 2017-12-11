8c358fa7a970f3440194d4efcf2eaa28aabcbef9
aventador <303639161@qq.com>
2017-12-10 23:12:38 -0500

    Promise Part 2

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { setTimeout } from 'timers';
 
 @Injectable()
 export class DishService {
@@ -8,15 +9,24 @@ export class DishService {
   constructor() { }
 
   getDishes(): Promise<Dish[]> {
-    return Promise.resolve(DISHES);
+    return new Promise(resolve => {
+      //Simulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES), 2000)
+    });
   }
 
   getDish(id: number): Promise<Dish>{
-    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
-  }
+    return new Promise(resolve => {
+      //Simulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+    })
+  };
 
   getFeaturedDish(): Promise<Dish>{
-    return Promise.resolve(DISHES.filter((dish) => (dish.featured))[0]);
+    return new Promise(resolve => {
+      //Simulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES.filter((dish) => (dish.featured))[0]), 2000);
+    });
   }
 
 }
